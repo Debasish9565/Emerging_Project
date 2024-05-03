b897ebf352c1ba83f1c1c60d4624cfabca2d230a
amanjot27 <singhamanjot069@gmail.com>
2024-05-03 20:14:51 +0530

    update

--- frontend/src/pages/Classes/Classes.jsx
+++ frontend/src/pages/Classes/Classes.jsx
@@ -3,8 +3,12 @@ import useaxiosFetch from "../../hooks/useAxiosFetch";
 
 const Classes = () => {
   const [classes, setClasses] = useState([]);
+  const [hoveredCard, setHoveredCard]= useState(null);
 
   const axiosFetch = useaxiosFetch();
+  const handleHover=(index) => {
+    setHoveredCard(index);
+  }
   useEffect(() => {
     axiosFetch
       .get("/classes")
@@ -18,6 +22,25 @@ const Classes = () => {
         <h1 className="text-4x1 font-bold text-center text-secondary">
           Classes
         </h1>
+      </div>
+
+      <div className='my-16 w-[90%] mx-auto grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 '>
+        {
+          classes.map((cls, index)=> (
+            <div
+            onMouseLeave={() => handleHover(null)}
+            key={index}
+            className={`relative hover:-translate-y-2 duration-150 hover:ring-[2px] hover:ring-secondary w-64 h-[350px] mx-auto  ${cls.availableSeats <1 ? 'bg-red-300': 'bg-white'} dark:bg-slate-600 rounded-lg shadow-lg overflow-hidden cursor-pointer`}
+            onMouseEnter={() => handleHover(index)}
+            >
+              <div className='relative h-48'>
+                <div className={`absolute inset-0 bg-black opacity-0 transition-opacity duration-300 ${hoveredCard == index ? "opacity-60" : ""}`}/>
+                <img src={cls.image} alt="" className='object-cover w-full h-full'/>
+
+              </div>
+            </div>
+          ))
+        }
       </div>
     </div>
   );
